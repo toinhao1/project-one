bb47e8c9b415dd005cf8c5b1f20b4af352a01128
toinhao1 <toinhao89@gmail.com>
2019-02-08 08:21:18 -0300

    Few package issues

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const staticController = require("../controllers/staticController");
+const staticController = require('../controllers/staticController');
 
-router.get("/", staticController.index);
-
+router.get('/', staticController.index);
 
 module.exports = router;
